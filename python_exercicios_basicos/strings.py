c3e71a306d6c7a59ab2a4f39656a35a146abb1fd
Lutzenberg <lutpax@gmail.com>
2026-01-04 01:05:30 -0300

    Adicionar exercícios de strings: concatenação e repetição

---
+++ python_exercicios_basicos/strings.py
@@ -0,0 +1,10 @@
+# Concatenar três strings separadas por espaço
+s1 = "ant"
+s2 = "bat"
+s3 = "cod"
+res = s1 + " " + s2 + " " + s3
+print(res)
+
+# Repetir uma string usando multiplicação (com espaço)
+res = (s1 + " ") * 10
+print(res)
