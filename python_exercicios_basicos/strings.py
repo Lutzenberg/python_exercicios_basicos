1e250cc0d82410ff5e2943fe6f024caabafbdcaa
Lutzenberg <lutpax@gmail.com>
2026-01-04 01:10:56 -0300

    Adicionar repetição de strings com diferentes quantidades

--- python_exercicios_basicos/strings.py
+++ python_exercicios_basicos/strings.py
@@ -6,5 +6,9 @@ res = s1 + " " + s2 + " " + s3
 print(res)
 
 # Repetir uma string usando multiplicação (com espaço)
-res = (s1 + " ") * 10
+res = (s1 + "") * 10
+print(res)
+
+# Repetir strings com espaços
+res = (s1 + " ") * 1 + (s2 + " ") * 2 + (s3 + " ") * 3
 print(res)
