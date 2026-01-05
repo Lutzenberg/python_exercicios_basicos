38ed8e97f67963e2d6710e16199ee829190f5b80
Lutzenberg <lutpax@gmail.com>
2026-01-05 01:40:05 -0300

    Adicionar entrada do usuário para produto e quantidade

--- exercicio_4/exercicio_4_1_1.py
+++ exercicio_4/exercicio_4_1_1.py
@@ -3,3 +3,6 @@ print("Escolha o que deseja comprar:")
 print("1 Maçã")
 print("2 Laranja")
 print("3 Banana")
+# Entrada do usuário
+produto = int(input("Qual sua escolha? "))
+qtd = int(input("Quantas unidades? "))
