685b0f76cc21cf008d385bc1f6382d56c09d21ff
Lutzenberg <lutpax@gmail.com>
2026-01-05 01:43:29 -0300

    Adicionar cálculo do total e verificação de produto

--- exercicio_4/exercicio_4_1_1.py
+++ exercicio_4/exercicio_4_1_1.py
@@ -6,3 +6,16 @@ print("3 Banana")
 # Entrada do usuário
 produto = int(input("Qual sua escolha? "))
 qtd = int(input("Quantas unidades? "))
+
+# Verificação do produto e cálculo do total
+if produto == 1:
+    pagar = qtd * 2.3
+    print(f"Você comprou {qtd} maçã(s). Total a pagar: R${pagar:.2f}")
+elif produto == 2:
+    pagar = qtd * 3.6
+    print(f"Você comprou {qtd} laranja(s). Total a pagar: R${pagar:.2f}")
+elif produto == 3:
+    pagar = qtd * 1.85
+    print(f"Você comprou {qtd} banana(s). Total a pagar: R${pagar:.2f}")
+else:
+    print("Produto inexistente!")
